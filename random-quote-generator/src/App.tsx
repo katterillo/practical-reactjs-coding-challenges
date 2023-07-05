383c308fc480ff9109d7db84d923fca94ae44194
Kattrina <kattrina.erillo@gmail.com>
2023-07-05 12:08:13 -0400

    Add hyperlinks to post on twitter and whatsapp

--- random-quote-generator/src/App.tsx
+++ random-quote-generator/src/App.tsx
@@ -60,20 +60,28 @@ function App() {
       <div className="container">
         <div className="quotation-box ">
           <Quotation />
-          <div className="quote">
-            {quoteList[index] ? <><p>{quoteList[index].quote}</p> <span>- {quoteList[index].author}</span></> : <p> Loading… </p>}
-          </div>
-          <div className="bottom-navigation">
-            <div>
-              <Button className={classnames("rotate cp")} onClick={decrement} />
-              <Button className="cp" onClick={increment} />
-            </div>
-            <div className="share">
-              <span>Share At:</span>
-              <Twitter title="Post this quote on twitter!" className="cp" />
-              <Whatsapp title="Post this quote on WhatsApp!" className="cp" />
-            </div>
-          </div>
+          {quoteList[index] ?
+            <>
+              <div className="quote">
+                <p>{quoteList[index].quote}</p> <span>- {quoteList[index].author}</span>
+              </div>
+              <div className="bottom-navigation">
+                <div>
+                  <Button className={classnames("rotate cp")} onClick={decrement} />
+                  <Button className="cp" onClick={increment} />
+                </div>
+                <div className="share">
+                  <span>Share At:</span>
+                  <a href={'https://twitter.com/intent/tweet?text=' + encodeURIComponent(quoteList[index].quote + " -" + quoteList[index].author)}>
+                    <Twitter title="Post this quote on twitter!" className="cp" />
+                  </a>
+                  <a href={'https://wa.me/?text=' + encodeURIComponent(quoteList[index].quote + " -" + quoteList[index].author)}>
+                    <Whatsapp title="Post this quote on WhatsApp!" className="cp" />
+                  </a>
+                </div>
+              </div>
+            </>
+            : <p> Loading… </p>}
         </div>
       </div >
       <div className="bottom-strip" />
