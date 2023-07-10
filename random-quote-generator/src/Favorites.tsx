4e057f62ba00c2333c600029302d75b5b225adec
Kattrina <kattrina.erillo@gmail.com>
2023-07-10 11:56:47 -0400

    Format favorite quotes section

--- random-quote-generator/src/Favorites.tsx
+++ random-quote-generator/src/Favorites.tsx
@@ -6,10 +6,11 @@ export default function Favorites(props: { favorites: string[], quoteList: Quote
     return (
         <>
             {/* TODO: optimize */}
+            <h3>Favorite Quotes</h3>
             {props.favorites.map((favorite, i) =>
                 props.quoteList.filter((quote, i) => (favorite === quote.quote))
                     .map(favoriteQuotes => (
-                        <p key={favoriteQuotes.quote}> {favoriteQuotes.quote}</p>
+                        <p key={favoriteQuotes.quote}> {favoriteQuotes.quote} -{favoriteQuotes.author}</p>
                     ))
             )}
         </>
