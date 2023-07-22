117147693e1e4260be9a6d8ea924d0eb8de1e01e
Kattrina <kattrina.erillo@gmail.com>
2023-07-22 13:36:18 -0400

    Optimize favorite quotes render

--- random-quote-generator/src/Favorites.tsx
+++ random-quote-generator/src/Favorites.tsx
@@ -3,16 +3,31 @@ import { Quotes } from './utils';
 
 export default function Favorites(props: { favorites: string[], quoteList: Quotes[] }) {
 
+    const quoteMap: { [key: string]: string } = {}; // Create an object to store the quotes as key-value pairs
+    props.quoteList.forEach(quote => {
+        quoteMap[quote.quote] = quote.author;
+    });
+
+    /* Demonstates how to create hashmap with string: string 
+   quoteMap = {
+        "To be or not to be": "William Shakespeare",
+        "Stay hungry, stay foolish": "Steve Jobs",
+        // Additional quotes...
+    };
+    */
+
+    // Now favoriteQuoteElements can be used in this component's render return statement.
+    const favoriteQuoteElements = props.favorites.map(favorite => (
+        quoteMap[favorite] && <p key={favorite}> {favorite} - {quoteMap[favorite]}</p>
+    ));
+
+    console.log("quoteMap: ", quoteMap);
     return (
         <>
-            {/* TODO: optimize */}
             <h3>Favorite Quotes</h3>
-            {props.favorites.map((favorite, i) =>
-                props.quoteList.filter((quote, i) => (favorite === quote.quote))
-                    .map(favoriteQuotes => (
-                        <p key={favoriteQuotes.quote}> {favoriteQuotes.quote} -{favoriteQuotes.author}</p>
-                    ))
-            )}
+            <div className="favorite-quotations-box">
+                {favoriteQuoteElements}
+            </div>
         </>
     )
 
